65d442df42715addcdf1419b0f8c2d684928ddcc
Cunha <JACU@ferring.com>
2018-04-19 11:36:49 -0400

    Added stalker and age calc JS

---
+++ Javascript/agecalc.js
@@ -0,0 +1,5 @@
+var age = prompt("How old are you?");
+var daysAlive = age*365;
+
+alert("You have been living for " + daysAlive + " days.");
+console.log("You have been living for roughly " + daysAlive + " days.");
