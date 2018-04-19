355eecdfae298158d39f9eb2f9cb95410504b79e
Cunha <JACU@ferring.com>
2018-04-19 10:40:56 -0400

    Javascript folder and first files added

---
+++ Javascript/jscheatlist.js
@@ -0,0 +1,33 @@
+//Single or Double quotes OK
+"hello world"
+'hello world'
+
+//COncatetation
+"charlie" + "brown"
+
+//Escape Characters start with "\"
+"Singin \"Do wah diddy, diddy, dum diddy do\" "
+"This is a backslash; \\"
+
+//Strings have a length property
+"hello world".length //11
+
+//Access individual characters using [] and an index
+"hello"[0] //h
+"hello"[1] //e
+
+//var will store all of the variable types
+var varName = value;
+
+//Variables that are declared but not initialized are undefined
+var name;
+
+//null is "explicitly nothing"
+var current = null;
+
+//METHODS
+clear() //clear console
+alert("Don't die on me!")
+console.log("This is being printed on the console")
+prompt("What is your name?") //prompt for user input
+var username = prompt("What is your name?") //prompt and save input from user
---
+++ Javascript/script.js
@@ -0,0 +1,4 @@
+var username = prompt("What is your name?");
+
+alert("Nice to meet you, " + username);
+console.log("Also great to meet you, " + username);
